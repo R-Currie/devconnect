62fb3d180a934ebc3e657da6da5a9ce7bc20962c
r-currie <currierobertm@gmail.com>
2019-12-10 21:02:17 -0600

    Database setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 
 app.get("/", function(req, res) {
   res.send("Hi");
@@ -8,4 +10,5 @@ app.get("/", function(req, res) {
 
 const PORT = process.env.PORT || 5000;
 
+//npm run server to start
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

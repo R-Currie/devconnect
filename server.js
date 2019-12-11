bc56aef7603b322c43d788763d50c8787e2a361a
r-currie <currierobertm@gmail.com>
2019-12-11 11:54:52 -0600

    Ssh test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hi");
 });
 
-//Define routes
+// Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));

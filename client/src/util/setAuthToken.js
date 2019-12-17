c33b3ff230e939df026c2834d213b6f468418d1d
r-currie <currierobertm@gmail.com>
2019-12-16 18:20:54 -0600

    auth action loads user and puts the token in header

---
+++ client/src/util/setAuthToken.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
+
+// Checks to see if there is a token and if there is, it will become the default and send it with every request
